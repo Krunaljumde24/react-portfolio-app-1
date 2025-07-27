7e39e38d554bf9d21cbca6341acf747169f357e4
Krunal Jumde <krunaljumde24@gmail.com>
2025-07-27 23:53:19 +0530

    moved images assets to public directory

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import logoPath from "../assets/tech/springboot.png";
-
 function Skills() {
   const [skills, setSkills] = useState([
     {
@@ -79,12 +77,12 @@ function Skills() {
   return (
     <div className="w-full dark:bg-emerald-950">
       <div className="w-4/5 mx-auto py-10">
-        <h2 className="text-3xl font-bold text-center pb-10 dark:text-white">Skills</h2>
+        <h2 className="text-3xl font-bold text-center pb-10 dark:text-white">
+          Skills
+        </h2>
         <div className="flex flex-wrap justify-center gap-4">
           {skills.map((item, index) => {
-            let imgPath = `${import.meta.env.BASE_URL}src/assets/tech/${
-              item.imgFileName
-            }`;
+            let imgPath = `/tech/${item.imgFileName}`;
             return (
               <div
                 key={index}
