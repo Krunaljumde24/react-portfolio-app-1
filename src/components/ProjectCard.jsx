2102ea7997f51b5562ef9d5c27fb8206facb92e9
Krunal Jumde <krunaljumde24@gmail.com>
2026-03-05 11:58:59 +0530

    updated customer-portal ui login url in project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function ProjectCard({ imgUrl, projectName }) {
+function ProjectCard({ imgUrl, projectName, appUrl }) {
+  console.log(projectName + " : " + appUrl);
   return (
-    <div className="max-w-xs bg-[white] border border-gray-200 rounded-lg shadow-sm
-     dark:bg-gray-700 dark:border-gray-700">
+    <div
+      className="max-w-xs bg-[white] border border-gray-200 rounded-lg shadow-sm
+     dark:bg-gray-700 dark:border-gray-700
+     transform transition-all duration-200 ease-out hover:scale-105 hover:shadow-xl
+     "
+    >
       <div className="p-4">
         <a href="#" className="">
           <img className="rounded-sm" src={imgUrl} alt="dukaan-project-img" />
@@ -16,7 +21,8 @@ function ProjectCard({ imgUrl, projectName }) {
           </h4>
         </a>
         <a
-          href="#"
+          href={appUrl}
+          target={appUrl === "#" ? "" : "_blank"}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Visit App
@@ -36,6 +42,18 @@ function ProjectCard({ imgUrl, projectName }) {
             />
           </svg>
         </a>
+        {/* {console.log(appUrl === "Customer Portal")} */}
+        {appUrl != "#" ? (
+          <div class="flex items-center gap-2">
+            <span class="w-3 h-3 bg-green-500 rounded-full"></span>
+            <span class="text-sm font-medium">Live</span>
+          </div>
+        ) : (
+          <div class="flex items-center gap-2">
+            <span class="w-3 h-3 bg-red-500 rounded-full"></span>
+            <span class="text-sm font-medium">Not Live</span>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,18 +10,22 @@ function Projects() {
     {
       name: "Dukaan - Online Grocery Shop",
       imgUrl: dukaan,
+      appUrl: "#",
     },
     {
       name: "Ecomm Application",
       imgUrl: ecomm,
+      appUrl: "#",
     },
     {
       name: "Customer Portal",
       imgUrl: cp,
+      appUrl: "https://customer-portal.hackerspace.co.in/",
     },
     {
       name: "Fitness Gym Application",
       imgUrl: fitness,
+      appUrl: "#",
     },
   ]);
   return (
@@ -29,7 +33,11 @@ function Projects() {
       <h2 className="text-3xl font-bold text-center">Projects</h2>
       <div className="flex justify-center gap-4 flex-wrap mx-auto py-10">
         {projects.map((obj) => (
-          <ProjectCard imgUrl={obj.imgUrl} projectName={obj.name} />
+          <ProjectCard
+            imgUrl={obj.imgUrl}
+            projectName={obj.name}
+            appUrl={obj.appUrl}
+          />
         ))}
       </div>
     </div>
